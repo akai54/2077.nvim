265ec834be5d58f270d795a5a16c6ec77f588d74
ALADDAM Besher <beshoux@outlook.fr>
2022-05-25 15:59:59 +0200

    First lualine try.

--- lua/lualine/themes/2077.lua
+++ lua/lualine/themes/2077.lua
@@ -3,34 +3,34 @@
 -- Credit: Zoltan Dalmadi(lightline)
 -- LuaFormatter off
 local colors = {
-	blue = "#569cd6",
-	green = "#6a9955",
-	purple = "#c586c0",
-	red1 = "#d16969",
-	yellow = "#dcdcaa",
-	orange = "#ce9178",
-	fg = "#d4d4d4",
-	bg = "#007acc",
-	-- bg     = '#68217a',
-	-- bg     = '#252525',
-	gray1 = "#5c6370",
-	gray2 = "#2c323d",
-	gray3 = "#3e4452",
+  blue = "#00c3ff",
+  green = "#00ffc8",
+  purple = "#ff0055",
+  red1 = "#ff007c",
+  yellow = "#FCEE0C",
+  orange = "#ffb800",
+  fg = "#e2dddf",
+  bg = "#03d8f3",
+  -- bg     = '#68217a',
+  -- bg     = '#252525',
+  gray1 = "#2f404d",
+  gray2 = "#3D898d",
+  gray3 = "#3e4452",
 }
 -- LuaFormatter on
 return {
-	normal = {
-		a = { fg = colors.fg, bg = colors.bg, gui = "bold" },
-		b = { fg = colors.fg, bg = colors.bg },
-		c = { fg = colors.fg, bg = colors.bg },
-	},
-	insert = { a = { fg = colors.fg, bg = colors.bg, gui = "bold" } },
-	visual = { a = { fg = colors.fg, bg = colors.bg, gui = "bold" } },
-	command = { a = { fg = colors.fg, bg = colors.bg, gui = "bold" } },
-	replace = { a = { fg = colors.fg, bg = colors.bg, gui = "bold" } },
-	inactive = {
-		a = { fg = colors.fg, bg = colors.bg },
-		b = { fg = colors.fg, bg = colors.bg },
-		c = { fg = colors.fg, bg = colors.bg },
-	},
+  normal = {
+    a = { fg = colors.fg, bg = colors.bg, gui = "bold" },
+    b = { fg = colors.fg, bg = colors.bg },
+    c = { fg = colors.fg, bg = colors.bg },
+  },
+  insert = { a = { fg = colors.fg, bg = colors.bg, gui = "bold" } },
+  visual = { a = { fg = colors.fg, bg = colors.bg, gui = "bold" } },
+  command = { a = { fg = colors.fg, bg = colors.bg, gui = "bold" } },
+  replace = { a = { fg = colors.fg, bg = colors.bg, gui = "bold" } },
+  inactive = {
+    a = { fg = colors.fg, bg = colors.bg },
+    b = { fg = colors.fg, bg = colors.bg },
+    c = { fg = colors.fg, bg = colors.bg },
+  },
 }
