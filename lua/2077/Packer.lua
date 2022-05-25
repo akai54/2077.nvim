b48ff65ac84adcd90ff2ea84d47cff53f612dce5
ALADDAM Besher <beshoux@outlook.fr>
2022-05-25 16:26:00 +0200

    Rechange for Packer.

--- lua/2077/Packer.lua
+++ lua/2077/Packer.lua
@@ -2,7 +2,7 @@ local Packer = {
   packerString = { fg = C.aureolin },
   packerHash = { fg = C.vivid_sky_blue },
   packerOutput = { fg = C.purple_test_2 },
-  packerRelDate = { fg = C.black },
+  packerRelDate = { fg = C.cc },
   packerSuccess = { fg = C.comment },
   packerStatusSuccess = { fg = C.vivid_sky_blue },
 }
