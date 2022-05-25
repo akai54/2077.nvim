9da2c1e3e7040654d064fe430d98b6e1b7af7731
ALADDAM Besher <beshoux@outlook.fr>
2022-05-25 15:25:26 +0200

    Add packer support.

--- lua/2077/Packer.lua
+++ lua/2077/Packer.lua
@@ -1,10 +1,11 @@
 local Packer = {
-		packerString = {fg = C.ui_orange, },
-		packerHash = {fg = C.ui4_blue, },
-		packerOutput = {fg = C.ui_purple, },
-		packerRelDate = {fg = C.gray, },
-		packerSuccess = {fg = C.success_green, },
-		packerStatusSuccess = {fg = C.ui4_blue, },
+  packerString = { fg = C.aureolin },
+  packerHash = { fg = C.vivid_sky_blue },
+  packerOutput = { fg = C.purple_test_2 },
+  packerRelDate = { fg = C.gray },
+  packerSuccess = { fg = C.comment },
+  packerStatusSuccess = { fg = C.vivid_sky_blue },
 }
 
 return Packer
+
