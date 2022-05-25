7da8a3901bf9ae1de309e5607c30b2c35e62e091
ALADDAM Besher <beshoux@outlook.fr>
2022-05-25 15:40:48 +0200

    Changing packer color scheme.

--- lua/2077/Packer.lua
+++ lua/2077/Packer.lua
@@ -2,10 +2,9 @@ local Packer = {
   packerString = { fg = C.aureolin },
   packerHash = { fg = C.vivid_sky_blue },
   packerOutput = { fg = C.purple_test_2 },
-  packerRelDate = { fg = C.gray },
+  packerRelDate = { fg = C.black },
   packerSuccess = { fg = C.comment },
   packerStatusSuccess = { fg = C.vivid_sky_blue },
 }
 
 return Packer
-
