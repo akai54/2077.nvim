798607c9b07c6a55279d5f3e107b1a34dab496e5
ALADDAM Besher <beshoux@outlook.fr>
2022-05-28 01:18:00 +0200

    First Telescope support.

--- lua/2077/Telescope.lua
+++ lua/2077/Telescope.lua
@@ -1,8 +1,7 @@
 local Telescope = {
-  TelescopeSelection = { fg = C.hint_blue },
-  TelescopeMatching = { fg = C.info_yellow, style = "bold" },
-  TelescopeBorder = { fg = C.blue, bg = Config.transparent_background and "NONE" or C.bg },
+  TelescopeSelection = { fg = C.vivid_sky_blue },
+  TelescopeMatching = { fg = C.comment, style = "bold" },
+  TelescopeBorder = { fg = C.purple_test_2, bg = Config.transparent_background and "NONE" or C.bg },
 }
 
 return Telescope
-
