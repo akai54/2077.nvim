0bbb26278cc8f4e0b338805538fb07cc03a25fe6
ALADDAM Besher <beshoux@outlook.fr>
2022-05-27 15:36:15 +0200

    More changes to the Packer.

--- lua/2077/Packer.lua
+++ lua/2077/Packer.lua
@@ -1,8 +1,8 @@
 local Packer = {
-  packerString = { fg = C.aureolin },
+  packerString = { fg = C.cc },
   packerHash = { fg = C.vivid_sky_blue },
   packerOutput = { fg = C.purple_test_2 },
-  packerRelDate = { fg = C.cc },
+  packerRelDate = { fg = C.aureolin },
   packerSuccess = { fg = C.comment },
   packerStatusSuccess = { fg = C.vivid_sky_blue },
 }
