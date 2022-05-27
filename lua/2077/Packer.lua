5c8971996e4dc26c60741c6ba50b22a0c728173c
ALADDAM Besher <beshoux@outlook.fr>
2022-05-28 01:38:06 +0200

    Test: changin color in packer.

--- lua/2077/Packer.lua
+++ lua/2077/Packer.lua
@@ -2,7 +2,7 @@ local Packer = {
   packerString = { fg = C.cc },
   packerHash = { fg = C.vivid_sky_blue },
   packerOutput = { fg = C.purple_test_2 },
-  packerRelDate = { fg = C.aureolin },
+  packerRelDate = { fg = C.gray },
   packerSuccess = { fg = C.comment },
   packerStatusSuccess = { fg = C.vivid_sky_blue },
 }
