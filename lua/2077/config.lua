2143d5a7d0b306c47e7316181549803219844f1c
ALADDAM Besher <beshoux@outlook.fr>
2022-05-28 03:37:02 +0200

    Update: Most italic set to false.

--- lua/2077/config.lua
+++ lua/2077/config.lua
@@ -15,10 +15,9 @@ end
 config = {
   transparent_background = opt("transparent_background", false),
   italic_comments = opt("italic_keywords", true) and "italic" or "NONE",
-  italic_keywords = opt("italic_keywords", true) and "italic" or "NONE",
+  italic_keywords = opt("italic_keywords", false) and "italic" or "NONE",
   italic_functions = opt("italic_function", false) and "italic" or "NONE",
-  italic_variables = opt("italic_variables", true) and "italic" or "NONE",
+  italic_variables = opt("italic_variables", false) and "italic" or "NONE",
 }
 
 return config
-
