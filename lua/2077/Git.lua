121751c32e832a94e70bb800e5723d4e420b6b44
ALADDAM Besher <beshoux@outlook.fr>
2022-05-30 01:11:16 +0200

    First Git signs support.

--- lua/2077/Git.lua
+++ lua/2077/Git.lua
@@ -1,10 +1,11 @@
 local Git = {
-		SignAdd = {fg = C.sign_add, },
-		SignChange = {fg = C.sign_change, },
-		SignDelete = {fg = C.sign_delete, },
-		GitSignsAdd = {fg = C.sign_add, },
-		GitSignsChange = {fg = C.sign_change, },
-		GitSignsDelete = {fg = C.sign_delete, },
+  SignAdd = { fg = C.comment },
+  SignChange = { fg = C.vivid_sky_blue },
+  SignDelete = { fg = C.purple_test_2 },
+  GitSignsAdd = { fg = C.comment },
+  GitSignsChange = { fg = C.vivid_sky_blue },
+  GitSignsDelete = { fg = C.purple_test_2 },
 }
 
 return Git
+
