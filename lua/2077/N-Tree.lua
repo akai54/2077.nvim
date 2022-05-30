0e3e8a963b24ddd3be93e217d60c8dab5a0a2b80
ALADDAM Besher <beshoux@outlook.fr>
2022-05-31 01:27:41 +0200

    First NvimTree support.

---
+++ lua/2077/N-Tree.lua
@@ -0,0 +1,26 @@
+local NvimTree = {
+  NvimTreeFolderIcon = { fg = C.vivid_sky_blue },
+  NvimTreeIndentMarker = { fg = C.co },
+  NvimTreeNormal = { fg = C.cc, bg = C.tree_gray },
+  NvimTreeVertSplit = { fg = C.purple_test_2, bg = C.purple_test_2 },
+  NvimTreeFolderName = { fg = C.vivid_sky_blue },
+  NvimTreeOpenedFolderName = { fg = C.vivid_sky_blue, style = "bold,italic" },
+  NvimTreeEmptyFolderName = { fg = C.gray_new, style = "italic" },
+  NvimTreeGitIgnored = { fg = C.gray, style = "italic" },
+  NvimTreeImageFile = { fg = C.cc },
+  NvimTreeSpecialFile = { fg = C.orange_new },
+  NvimTreeEndOfBuffer = { fg = C.blanc },
+  NvimTreeCursorLine = { bg = C.co },
+  NvimTreeGitignoreIcon = { fg = C.red_new },
+  NvimTreeGitStaged = { fg = C.comment },
+  NvimTreeGitNew = { fg = C.comment },
+  NvimTreeGitRenamed = { fg = C.comment },
+  NvimTreeGitDeleted = { fg = C.purple_test_2 },
+  NvimTreeGitMerge = { fg = C.aurelion },
+  NvimTreeGitDirty = { fg = C.aurelion },
+  NvimTreeSymlink = { fg = C.cyan },
+  NvimTreeRootFolder = { fg = C.gray_new, style = "bold" },
+  NvimTreeExecFile = { fg = C.cyan },
+}
+
+return NvimTree
